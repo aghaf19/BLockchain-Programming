0b11242dfe9734bf46498e5bc9207278f9744d6f
aghaf19 <60296005+aghaf19@users.noreply.github.com>
2020-12-22 16:00:20 +0500

    Delete contact.js

--- contact.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express")
-const express = require("express")
-const router = express.Router()
-router.get("/",(req,res)=>
-{
-    res.send("welcome to contact page ")
-})
-module.exports= router;
